1d653b7d9268ed9829ac01ae4dc810c468dce861
bmhv95 <131663255+bmhv95@users.noreply.github.com>
2023-08-08 19:21:36 +0700

    First commit

---
+++ src/main/java/org/financier/v1/config/RestConfiguration.java
@@ -0,0 +1,8 @@
+package org.financier.v1.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class RestConfiguration extends Application {
+}
---
+++ src/main/java/org/financier/v1/rest/HelloWorld.java
@@ -0,0 +1,14 @@
+package org.financier.v1.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/hello")
+public class HelloWorld {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello World";
+    }
+}
